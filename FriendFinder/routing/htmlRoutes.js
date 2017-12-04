edd05bc3a794e3086cc80ddd5cbc30d8f8d4cb8d
Ren <rjingsiu@gmail.com>
2017-12-04 08:43:44 -0800

    added dependencies and basic routes

---
+++ FriendFinder/routing/htmlRoutes.js
@@ -0,0 +1,24 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// Routes 
+// Basic route
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname, "survey.html"));
+});
+
