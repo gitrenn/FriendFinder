6bf80cb0bbbd89ecf43194a9b67eecb8cedb4e16
Ren <rjingsiu@gmail.com>
2017-12-04 23:55:12 -0800

    changed layout and some componets

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
     {
         "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://picsum.photos/200/300?image=1069",
         "scores":[
             5,
             1,
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -2,13 +2,7 @@
 
 var path = require("path");
 
-
-// Sets up the Express app to handle data parsing
-
-
-
 // Routes 
-// Basic route
 
 module.exports = function(app) {
     app.get("/", function(req, res){
